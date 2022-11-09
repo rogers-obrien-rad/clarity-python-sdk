5fd36d0ca1b3877828dbcdd16ed8e09dba2d9eb6
Hagen Fritz <hagenfritz@gmail.com>
2022-11-09 10:13:16 -0600

    show basic usage

---
+++ snippets/get_measurements.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import pathlib
+sys.path.append(f"{pathlib.Path(__file__).resolve().parent.parent}")
+
+from claripy.clarity import Clarity
+
+from dotenv import load_dotenv
+
+if os.getenv("API_KEY") is None:
+    load_dotenv()
+
+if __name__ == "__main__":
+    connection = Clarity(os.getenv("API_KEY"))
+
+    print(connection.__measurements__.get())
