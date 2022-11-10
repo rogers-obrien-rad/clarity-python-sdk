4dae7d7c4dd996f035888ba3fd6500cfd28a6cb4
Hagen Fritz <hagenfritz@gmail.com>
2022-11-10 15:31:33 -0600

    update get_request to take query params

--- claripy/access/base.py
+++ claripy/access/base.py
@@ -7,7 +7,7 @@ class Base:
         self.__api_key = api_key
         self.base_url = "https://clarity-data-api.clarity.io"
 
-    def get_request(self, api_url):
+    def get_request(self, api_url, params=None):
         """
         Create a HTTP Get request
 
@@ -15,19 +15,24 @@ class Base:
         ----------
         api_url : str
             endpoint for the specific API call
+        params : dict, default None
+            GET query parameters to parse
 
         Returns
         -------
+        response : dict
+            GET response in json
         """
-        url = f"{self.base_url}{api_url}"
         headers = {"x-api-key": f"{self.__api_key}"}
+        
+        if params is not None:
+            url = f"{self.base_url}{api_url}?{urllib.parse.urlencode(params)}"
+        else:
+            url = f"{self.base_url}{api_url}"
 
         response = requests.get(
             url,
             headers=headers
         )
 
-        if response.ok:
-            return response.json()
-        else:
-            return response.status_code
+        return response
